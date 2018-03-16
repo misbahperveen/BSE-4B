60587e49c9085da850d960b218ca82b72a3c8434
Bahria University <spring2018@bahria.edu.pk>
2018-03-16 09:16:35 +0500

    First Commit

---
+++ ChainOfResponsibilities/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibilities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Handler obj = new ConcreateHandler();
+            Handler obj1 = new ConcreateHandler1();
+            Handler obj2 = new ConcreateHandler2();
+            obj.SetSuccessor(obj1);
+            obj1.SetSuccessor(obj2);
+
+            obj.RequestHandler(12);
+            obj.RequestHandler(5);
+            obj.RequestHandler(21);
+        }
+    }
+
+
+    abstract class Handler
+    {
+        protected Handler successor;
+
+        public abstract void RequestHandler(int val);
+
+        public void SetSuccessor(Handler obj)
+        {
+            successor = obj;
+        }
+    }
+
+    class ConcreateHandler : Handler
+    {
+        public override void RequestHandler(int val)
+        {
+            if (val > 0 && val < 10)
+            {
+                Console.WriteLine("Request Handled "+ this.GetType().ToString());
+            }
+            else
+            {
+                successor.RequestHandler(val);
+            }
+        }
+    }
+
+    class ConcreateHandler1 : Handler
+    {
+        public override void RequestHandler(int val)
+        {
+            if (val > 10 && val < 20)
+            {
+                Console.WriteLine("Request Handled " + this.GetType().ToString());
+            }
+            else
+            {
+                successor.RequestHandler(val);
+            }
+        }
+    }
+
+    class ConcreateHandler2 : Handler
+    {
+        public override void RequestHandler(int val)
+        {
+            if (val > 20 && val < 30)
+            {
+                Console.WriteLine("Request Handled " + this.GetType().ToString());
+            }
+            else
+            {
+                successor.RequestHandler(val);
+            }
+        }
+    }
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Component obj = new ConcreteComponent();
+            obj.Operation();
+            Decorator objDecorator = new ConcreteDecorator();
+            objDecorator.SetComponent(obj);
+            objDecorator.Operation();
+            Decorator objDecorator2 = new ConcreteDecorator2();
+            objDecorator2.SetComponent(objDecorator);
+            objDecorator2.Operation();
+        }
+    }
+
+    abstract class Component {
+        public abstract void Operation();
+
+    }
+
+    class ConcreteComponent : Component
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("ConcreteComponent");
+        }
+    }
+
+    class Decorator : Component
+    {
+        protected Component component;
+        public void SetComponent(Component obj) { component = obj; }
+        public override void Operation()
+        {
+            Console.WriteLine("Decorator");
+        }
+    }
+
+    class ConcreteDecorator : Decorator
+    {
+        public void AddonFunctionality() {
+            Console.WriteLine("AddonFunctionality");
+        }
+        public override void Operation()
+        {
+            AddonFunctionality();
+            Console.WriteLine(GetType().ToString());
+        }
+
+    }
+
+    class ConcreteDecorator2 : Decorator
+    {
+        public override void Operation()
+        {
+            AddonFunctionality2();
+            Console.WriteLine(GetType().ToString());
+         }
+        public void AddonFunctionality2() {
+            Console.WriteLine("AddonFunctionality2");
+        }
+
+    }
+
+}
